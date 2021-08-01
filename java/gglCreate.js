001cbfe7a64dc7e73052110449bbcd9ec5f034ce
jefedeoro <83683076+jefedeoro@users.noreply.github.com>
2021-08-01 16:25:57 -0500

    Create gglCreate.js

---
+++ java/gglCreate.js
@@ -0,0 +1,40 @@
+/* Step 1 
+        -optional user logging 
+        -update Person class
+*/
+
+
+
+
+/* Step 2 
+        - user[0] uploads files, and adds other userId's involved in the file.
+        - update UploadedDocs class for each document 
+        - user[0] signs document
+        - sends to user[1] for approval
+*/
+
+
+
+
+/* Step 3 
+        - sends notification to user[1]  account
+        - user[1] verifies the files and signs
+        - signature sent to user[0] to verify user[1] signature
+        - if more users send to next and return to all signed to verify 
+*/
+
+/* Step 4 
+        - All Person(s) involved verify all signatures. 
+        - All UploadedDocs arranged on a single page html 
+        - Send html code to bitty
+        - Send bitty URL to urlShrinker
+        - (optional) create NFT send to all users walletAddress
+*/
+
+
+/* Step 5 
+        - if options are added send reward to user[0] by default
+        - options to split reward to all users 
+        - rewards are paid in native token
+
+
